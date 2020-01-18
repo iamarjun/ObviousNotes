0b68247d1728151a3f87c6b6c93336a96be9fef8
iamarjun <arjunmanoj1995@gmail.com>
2020-01-18 13:53:14 +0530

    setup bottom app bar

---
+++ app/src/main/java/com/example/obviousnotes/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.obviousnotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
